0a7a4392f32103f918bfdb3ba379008fa92ff6b5
Amir Shahmoradi <a.shahmoradi@gmail.com>
2020-06-07 04:13:48 -0500

    minor enhancements

--- install.all.macos.sh
+++ install.all.macos.sh
@@ -1,2 +1,2 @@
-./install.sh --mem heap --lib dynamic --par "none mpi" -s gnu >install.sh.gnu.out 2>&1
+./install.sh --mem heap --lib dynamic --par "none mpi" -s gnu >install.sh.gnu.out 2>&1 
 ./install.sh --mem heap --lib dynamic --par "none" -s intel >install.sh.intel.out 2>&1
