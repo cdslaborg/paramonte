6f207e00f1a30d4e09f452ee388d82c9817b2743
Amir Shahmoradi <a.shahmoradi@gmail.com>
2020-09-06 03:04:49 -0500

    minor enhancements

--- auxil/getCompilerVersion.f90
+++ auxil/getCompilerVersion.f90
@@ -23,41 +23,135 @@
 !**********************************************************************************************************************************
 
 program getCompilerVersion
-use iso_fortran_env, only: compiler_version
-implicit none
-integer :: fileunit, startindex, endindex
-logical :: isIntel = .false.
-logical :: isGNU = .false.
-character(:), allocatable :: string, isParaMonteCompatibleCompiler
 
-isParaMonteCompatibleCompiler = "false"
-string = trim(adjustl(compiler_version()))
+    use iso_fortran_env, only: compiler_version, IK => int32
+    implicit none
+    integer(IK) , parameter     :: INTEL_VERSION = [18_IK,0_IK,0_IK]
+    integer(IK) , parameter     :: GNU_VERSION = [7_IK,3_IK,0_IK]
+    integer(IK)                 :: fileunit, startindex, endindex
+    logical                     :: isIntel = .false.
+    logical                     :: isGNU = .false.
+    character(:), allocatable   :: string, isParaMonteCompatibleCompiler
 
-if ( index(string,"GCC") > 0 ) then ! it's Gfortran
-    startindex = index(string,"version") + 8
-    endindex = len(string)
-    isGNU = .true.
-elseif ( index(string,"Intel") > 0 ) then ! it's Intel
-    startindex = index(string,"Version") + 8
-    endindex = index(string,"Build") - 2
-    isIntel = .true.
-end if
+    type :: CharVec_type
+        character(:)    , allocatable   :: record
+    end type CharVec_type
 
-string = trim(adjustl(string(startindex:endindex)))
-open(newunit=fileunit,file="getCompilerVersion.tmp")
-write(fileunit,"(A)") string
-close(fileunit)
+    type(CharVec_type)  , allocatable   :: VersionParts
 
-! check for ParaMonteCompatibleCompiler
+    isParaMonteCompatibleCompiler = "false"
+    string = trim(adjustl(compiler_version()))
 
-if (isIntel) then
-    if ( lge(string(1:6),"18.0.0") ) isParaMonteCompatibleCompiler = "true"
-elseif (isGNU) then
-    if ( lge(string(1:3),"7.3") ) isParaMonteCompatibleCompiler = "true"
-end if
+    if ( index(string,"GCC") > 0 ) then ! it's Gfortran
+        startindex = index(string,"version") + 8
+        endindex = len(string)
+        isGNU = .true.
+    elseif ( index(string,"Intel") > 0 ) then ! it's Intel
+        startindex = index(string,"Version") + 8
+        endindex = index(string,"Build") - 2
+        isIntel = .true.
+    end if
 
-open(newunit=fileunit,file="isParaMonteCompatibleCompiler.tmp")
-write(fileunit,"(A)") isParaMonteCompatibleCompiler
-close(fileunit)
+    string = trim(adjustl(string(startindex:endindex)))
+    open(newunit=fileunit,file="getCompilerVersion.tmp")
+    write(fileunit,"(A)") string
+    close(fileunit)
+
+    ! check for ParaMonteCompatibleCompiler
+
+    VersionParts = splitStr(string, ".")
+    if (isIntel) then
+        if ( str2int(VersionParts(1)%record) >= INTEL_VERSION(1) .and. str2int(VersionParts(2)%record) >= INTEL_VERSION(2) ) isParaMonteCompatibleCompiler = "true"
+        !if ( lge(string(1:6),"18.0.0") ) isParaMonteCompatibleCompiler = "true"
+    elseif (isGNU) then
+        if ( str2int(VersionParts(1)%record) >= GNU_VERSION(1) .and. str2int(VersionParts(2)%record) >= GNU_VERSION(2) ) isParaMonteCompatibleCompiler = "true"
+        !if ( lge(string(1:3),"7.3") ) isParaMonteCompatibleCompiler = "true"
+    end if
+
+    open(newunit=fileunit,file="isParaMonteCompatibleCompiler.tmp")
+    write(fileunit,"(A)") isParaMonteCompatibleCompiler
+    close(fileunit)
+
+contains
+
+    function splitStr(string,delimiter,nPart) result(Parts)
+
+        implicit none
+        character(len=*)    , intent(in)            :: string, delimiter
+        integer(IK)         , intent(out), optional :: nPart
+        character(len=:)    , allocatable           :: dummyStr
+        type(CharVec_type)  , allocatable           :: Parts(:)
+        integer(IK)                                 :: maxNumSplit
+        integer(IK)                                 :: stringLen, delimLen, splitCounter, currentPos
+
+        dummyStr  = string
+        delimLen  = len(delimiter)
+        stringLen = len(dummyStr)
+
+        if (delimLen==0) then
+            allocate(Parts(1))
+            Parts(1)%record = string
+            return
+        end if
+
+        maxNumSplit = 1 + stringLen / delimLen
+        allocate(Parts(maxNumSplit))
+        splitCounter = 1
+        loopParseString: do
+            if (stringLen<delimLen) then
+                Parts(splitCounter)%record = dummyStr
+                exit loopParseString
+            elseif (stringLen==delimLen) then
+                if (dummyStr==delimiter) then
+                    Parts(splitCounter)%record = ""
+                else
+                    Parts(splitCounter)%record = dummyStr
+                end if
+                exit loopParseString
+            elseif (dummyStr(1:delimLen)==delimiter) then
+                dummyStr = dummyStr(delimLen+1:stringLen)
+                stringLen = len(dummyStr)
+                cycle loopParseString
+            else
+                currentPos = 2
+                loopSearchString: do
+                    if (dummyStr(currentPos:currentPos+delimLen-1)==delimiter) then
+                        Parts(splitCounter)%record = dummyStr(1:currentPos-1)
+                        if (currentPos+delimLen>stringLen) then
+                            exit loopParseString
+                        else
+                            splitCounter = splitCounter + 1
+                            dummyStr = dummyStr(currentPos+delimLen:stringLen)
+                            stringLen = len(dummyStr)
+                            cycle loopParseString
+                        end if
+                    else
+                        currentPos = currentPos + 1
+                        if (stringLen<currentPos+delimLen-1) then
+                            Parts(splitCounter)%record = dummyStr
+                            exit loopParseString
+                        end if
+                        cycle loopSearchString
+                    end if
+                end do loopSearchString
+            end if
+        end do loopParseString
+        Parts = Parts(1:splitCounter)
+        if (present(nPart)) nPart = splitCounter
+
+    end function splitStr
+
+    function str2int(str,iostat)
+        implicit none
+        character(*), intent(in)        :: str
+        integer, optional, intent(out)  :: iostat
+        integer(IK)                     :: str2int
+        if (present(iostat)) then
+            iostat = 0
+            read(str,*,iostat=iostat) str2int
+        else
+            read(str,*) str2int
+        endif
+    end function str2int
 
 end program getCompilerVersion
