e47a9237dadb6b4329283c05237c52d180c5d9e6
Amir Shahmoradi <a.shahmoradi@gmail.com>
2020-09-08 23:30:22 -0500

    minor enhancements

--- auxil/getCompilerVersion.f90
+++ auxil/getCompilerVersion.f90
@@ -1,26 +1,42 @@
-!**********************************************************************************************************************************
-!**********************************************************************************************************************************
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 !
-!  ParaMonte: plain powerful parallel Monte Carlo library.
+!   MIT License
 !
-!  Copyright (C) 2012-present, The Computational Data Science Lab
+!   ParaMonte: plain powerful parallel Monte Carlo library.
 !
-!  This file is part of ParaMonte library. 
+!   Copyright (C) 2012-present, The Computational Data Science Lab
 !
-!  ParaMonte is free software: you can redistribute it and/or modify
-!  it under the terms of the GNU Lesser General Public License as published by
-!  the Free Software Foundation, version 3 of the License.
+!   This file is part of the ParaMonte library.
 !
-!  ParaMonte is distributed in the hope that it will be useful,
-!  but WITHOUT ANY WARRANTY; without even the implied warranty of
-!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!  GNU Lesser General Public License for more details.
+!   Permission is hereby granted, free of charge, to any person obtaining a 
+!   copy of this software and associated documentation files (the "Software"), 
+!   to deal in the Software without restriction, including without limitation 
+!   the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+!   and/or sell copies of the Software, and to permit persons to whom the 
+!   Software is furnished to do so, subject to the following conditions:
 !
-!  You should have received a copy of the GNU Lesser General Public License
-!  along with ParaMonte.  If not, see <https://www.gnu.org/licenses/>.
+!   The above copyright notice and this permission notice shall be 
+!   included in all copies or substantial portions of the Software.
 !
-!**********************************************************************************************************************************
-!**********************************************************************************************************************************
+!   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+!   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+!   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+!   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+!   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+!   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+!   OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+!
+!   ACKNOWLEDGMENT
+!
+!   ParaMonte is an honor-ware and its currency is acknowledgment and citations.
+!   As per the ParaMonte library license agreement terms, if you use any parts of 
+!   this library for any purposes, kindly acknowledge the use of ParaMonte in your 
+!   work (education/research/industry/development/...) by citing the ParaMonte 
+!   library as described on this page:
+!
+!       https://github.com/cdslaborg/paramonte/blob/master/ACKNOWLEDGMENT.md
+!
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 program getCompilerVersion
 
--- src/interface/MATLAB/paramonte/auxil/functions/isGUI.m
+++ src/interface/MATLAB/paramonte/auxil/functions/isGUI.m
@@ -1,37 +1,45 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%   ParaMonte: plain powerful parallel Monte Carlo library.
-%
-%   Copyright (C) 2012-present, The Computational Data Science Lab
-%
-%   This file is part of the ParaMonte library.
-%
-%   ParaMonte is free software: you can redistribute it and/or modify it 
-%   under the terms of the GNU Lesser General Public License as published 
-%   by the Free Software Foundation, version 3 of the License.
-%
-%   ParaMonte is distributed in the hope that it will be useful,
-%   but WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-%   GNU Lesser General Public License for more details.
-%
-%   You should have received a copy of the GNU Lesser General Public License
-%   along with the ParaMonte library. If not, see, 
-%
-%       https://github.com/cdslaborg/paramonte/blob/master/LICENSE
-%
-%   ACKNOWLEDGMENT
-%
-%   As per the ParaMonte library license agreement terms, 
-%   if you use any parts of this library for any purposes, 
-%   we ask you to acknowledge the use of the ParaMonte library
-%   in your work (education/research/industry/development/...)
-%   by citing the ParaMonte library as described on this page:
-%
-%       https://github.com/cdslaborg/paramonte/blob/master/ACKNOWLEDGMENT.md
-%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+%%%%
+%%%%   MIT License
+%%%%
+%%%%   ParaMonte: plain powerful parallel Monte Carlo library.
+%%%%
+%%%%   Copyright (C) 2012-present, The Computational Data Science Lab
+%%%%
+%%%%   This file is part of the ParaMonte library.
+%%%%
+%%%%   Permission is hereby granted, free of charge, to any person obtaining a 
+%%%%   copy of this software and associated documentation files (the "Software"), 
+%%%%   to deal in the Software without restriction, including without limitation 
+%%%%   the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+%%%%   and/or sell copies of the Software, and to permit persons to whom the 
+%%%%   Software is furnished to do so, subject to the following conditions:
+%%%%
+%%%%   The above copyright notice and this permission notice shall be 
+%%%%   included in all copies or substantial portions of the Software.
+%%%%
+%%%%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+%%%%   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+%%%%   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+%%%%   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+%%%%   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+%%%%   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+%%%%   OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%%%%
+%%%%   ACKNOWLEDGMENT
+%%%%
+%%%%   ParaMonte is an honor-ware and its currency is acknowledgment and citations.
+%%%%   As per the ParaMonte library license agreement terms, if you use any parts of 
+%%%%   this library for any purposes, kindly acknowledge the use of ParaMonte in your 
+%%%%   work (education/research/industry/development/...) by citing the ParaMonte 
+%%%%   library as described on this page:
+%%%%
+%%%%       https://github.com/cdslaborg/paramonte/blob/master/ACKNOWLEDGMENT.md
+%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function guiEnabled = isGUI()
     persistent guiIsEnabled
     if isempty(guiIsEnabled)
