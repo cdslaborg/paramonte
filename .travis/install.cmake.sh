dce4263835170a2c28f54d0e1c3deed8b592f786
Amir Shahmoradi <a.shahmoradi@gmail.com>
2020-11-09 00:31:55 -0600

    minor enhancements

---
+++ .travis/install.cmake.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install build-essential libssl-dev
+wget https://github.com/Kitware/CMake/releases/download/v3.16.5/cmake-3.16.5.tar.gz
+tar -zxvf cmake-3.16.5.tar.gz
+cd cmake-3.16.5
+./bootstrap
+make 
+sudo make install
