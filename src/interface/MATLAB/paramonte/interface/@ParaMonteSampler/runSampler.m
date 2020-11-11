1de471d328f4ed723e4738d6bdf3545288ab0b40
Amir Shahmoradi <a.shahmoradi@gmail.com>
2020-11-11 03:46:10 -0600

    minor enhancements

--- src/interface/MATLAB/paramonte/interface/@ParaMonteSampler/runSampler.m
+++ src/interface/MATLAB/paramonte/interface/@ParaMonteSampler/runSampler.m
@@ -9,30 +9,30 @@
 %%%%
 %%%%   This file is part of the ParaMonte library.
 %%%%
-%%%%   Permission is hereby granted, free of charge, to any person obtaining a 
-%%%%   copy of this software and associated documentation files (the "Software"), 
-%%%%   to deal in the Software without restriction, including without limitation 
-%%%%   the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-%%%%   and/or sell copies of the Software, and to permit persons to whom the 
+%%%%   Permission is hereby granted, free of charge, to any person obtaining a
+%%%%   copy of this software and associated documentation files (the "Software"),
+%%%%   to deal in the Software without restriction, including without limitation
+%%%%   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+%%%%   and/or sell copies of the Software, and to permit persons to whom the
 %%%%   Software is furnished to do so, subject to the following conditions:
 %%%%
-%%%%   The above copyright notice and this permission notice shall be 
+%%%%   The above copyright notice and this permission notice shall be
 %%%%   included in all copies or substantial portions of the Software.
 %%%%
-%%%%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-%%%%   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-%%%%   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-%%%%   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-%%%%   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-%%%%   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+%%%%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%%%%   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+%%%%   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+%%%%   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+%%%%   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+%%%%   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 %%%%   OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %%%%
 %%%%   ACKNOWLEDGMENT
 %%%%
 %%%%   ParaMonte is an honor-ware and its currency is acknowledgment and citations.
-%%%%   As per the ParaMonte library license agreement terms, if you use any parts of 
-%%%%   this library for any purposes, kindly acknowledge the use of ParaMonte in your 
-%%%%   work (education/research/industry/development/...) by citing the ParaMonte 
+%%%%   As per the ParaMonte library license agreement terms, if you use any parts of
+%%%%   this library for any purposes, kindly acknowledge the use of ParaMonte in your
+%%%%   work (education/research/industry/development/...) by citing the ParaMonte
 %%%%   library as described on this page:
 %%%%
 %%%%       https://github.com/cdslaborg/paramonte/blob/master/ACKNOWLEDGMENT.md
@@ -248,37 +248,52 @@ function runSampler(self,ndim,getLogFunc,varargin)
 
     isGNU = contains(self.libName,"gnu");
     if isGNU
-        setenv('GFORTRAN_STDIN_UNIT' , '5') 
-        setenv('GFORTRAN_STDOUT_UNIT', '6') 
+        setenv('GFORTRAN_STDIN_UNIT' , '5')
+        setenv('GFORTRAN_STDOUT_UNIT', '6')
         setenv('GFORTRAN_STDERR_UNIT', '0')
     end
     munlock(self.libName)
     eval("clear "+self.libName);
     simFailed = false;
+    %eval(expression);
     try
         eval(expression);
-    catch
-        simFailed = true;
+    catch ME
+        self.Err.msg = "Error occurred: " + string(ME.message) + newline + newline;
+        if ismac && strcmpi(ME.identifier,'MATLAB:mex:ErrInvalidMEXFile')
+            self.Err.msg = self.Err.msg ...
+                         + "This error is most likely due to the ""System Integrity Protection"" (SIP) of your Mac interfering with the ParaMonte MATLAB files." + newline ...
+                         + "Follow the directions given on this website to resolve this error:" + newline ...
+                         + newline ...
+                         + "    " + href(self.website.home.url + "/notes/troubleshooting/macos-developer-cannot-be-verified/") + newline ...
+                         + newline ...
+                         + "If the problem persists even after following the guidelines in the above page, please report this issue at," + newline ...
+                         + newline ...
+                         + "    " + href(self.website.github.issues.url) ...
+                         ;
+        else
+           if self.mpiEnabled
+                reportFileSuffix = "_process_*_report.txt";
+            else
+                reportFileSuffix = "_process_1_report.txt";
+            end
+            self.Err.msg    = "The " + self.methodName + " simulation failed. Please see the contents of the simulation's output report file(s) for potential diagnostic messages:" + newline + newline ...
+                            + "    " + strrep(self.spec.outputFileName+reportFileSuffix,'\','\\') ...
+                            ;
+        end
+        self.Err.marginTop = 1;
+        self.Err.wrapSplit = ": "; % important
+        self.Err.abort();
     end
+
     munlock(self.libName)
     eval("clear "+self.libName);
     if isGNU
-        setenv('GFORTRAN_STDIN_UNIT' , '-1') 
-        setenv('GFORTRAN_STDOUT_UNIT', '-1') 
+        setenv('GFORTRAN_STDIN_UNIT' , '-1')
+        setenv('GFORTRAN_STDOUT_UNIT', '-1')
         setenv('GFORTRAN_STDERR_UNIT', '-1')
     end
 
-    if simFailed
-        if self.mpiEnabled
-            reportFileSuffix = "_process_*_report.txt";
-        else
-            reportFileSuffix = "_process_1_report.txt";
-        end
-        self.Err.msg    = "The " + self.methodName + " simulation failed. Please see the contents of the simulation's output report file(s) for potential diagnostic messages:" + newline + newline ...
-                        + "    " + strrep(self.spec.outputFileName+reportFileSuffix,'\','\\') ...
-                        ;
-        self.Err.abort();
-    end
     %try
     %    eval(expression);
     %catch
