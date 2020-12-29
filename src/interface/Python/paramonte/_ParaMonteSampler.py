9b3664909c61324562a16108da8930bb4b3a9de1
Amir Shahmoradi <a.shahmoradi@gmail.com>
2020-12-29 15:18:15 -0600

    minor enhancements

--- src/interface/Python/paramonte/_ParaMonteSampler.py
+++ src/interface/Python/paramonte/_ParaMonteSampler.py
@@ -579,10 +579,9 @@ class ParaMonteSampler:
 
                 libName = libNamePrefix + pmcs + "_" + buildMode + "_dynamic_heap" + parallelism + libNameSuffix
                 libPath = find_library(libName)
-
-                if libPath is not None:
-                    libFound = os.path.isfile(libPath)
-                    if libFound: break
+                if libPath is None: libPath = os.path.join( pm.path.lib, libName )
+                libFound = os.path.isfile(libPath)
+                if libFound: break
 
             # exist the loop if the library has been found
 
