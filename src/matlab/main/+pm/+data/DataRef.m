0aba4804107c7388a94773802f59034557e6be79
Amir Shahmoradi <shahmoradi@utexas.edu>
2024-05-17 18:47:24 -0500

    matlab documentation enhancements.

--- src/matlab/main/+pm/+data/DataRef.m
+++ src/matlab/main/+pm/+data/DataRef.m
@@ -1,54 +1,51 @@
+%>
+%>  \brief
+%>  This is the abstract class for generating instances of objects
+%>  that can contain basic attributes required for storing reference
+%>  to a given data.
+%>
+%>  \details
+%>  This class is merely a convenience read-only
+%>  wrapper to reference external data.
+%>
+%>  \param[in]  dfref   :   The input object containing the target dataset
+%>                          or function handle that takes no arguments and returns the dataset.<br>
+%>                          Specifying a function handle is superior to specifying the dataset
+%>                          directly, because the function handle will always allow the use of
+%>                          the most updated version of the user table or matrix.<br>
+%>                          (**optional**. default = ``[]``)
+%>
+%>  \return
+%>  `self`              :   The output scalar object of class ``pm.data.DataRef``.
+%>
+%>  \interface{DataRef}
+%>  \code{.m}
+%>
+%>      df = pm.data.DataRef(dfref);
+%>
+%>  \endcode
+%>
+%>  \note
+%>  This class primarily exist to facilitate bypassing
+%>  the lack of references and pointers in MATLAB.<br>
+%>
+%>  \see
+%>  [DataFrame](@ref DataFrame)<br>
+%>  See the class attributes descriptions.<br>
+%>
+%>  \finmain{DataRef}
+%>
+%>  \author
+%>  \AmirShahmoradi, Tuesday March 7, 2017, 7:03 AM, Institute for Computational Engineering and Sciences (ICES), The University of Texas at Austin
 classdef DataRef < pm.matlab.Handle
-    %
-    %   This is the abstract class for generating instances of objects
-    %   that can contain basic attributes required for storing reference
-    %   to a given data.
-    %
-    %   This class is merely a convenience read-only
-    %   wrapper to reference external data.
-    %
-    %   This class primarily exist to facilitate bypassing
-    %   the lack of references and pointers in MATLAB.
-    %
-    %   Parameters
-    %   ----------
-    %
-    %       dfref
-    %
-    %           The input object containing the target dataset
-    %           or function handle that takes no arguments and returns the dataset.
-    %           Specifying a function handle is superior to specifying the dataset
-    %           directly, because the function handle will always allow the use of
-    %           the most updated version of the user table or matrix.
-    %           (**optional**. default = ``[]``)
-    %
-    %   Returns
-    %   -------
-    %
-    %       self
-    %
-    %           The output scalar object of class ``pm.data.DataRef``.
-    %
-    %   Interface
-    %   ---------
-    %
-    %       df = pm.data.DataRef(dfref);
-    %
-    %   Attributes
-    %   ----------
-    %
-    %       See the class attributes descriptions below.
-    %
     properties(Access = protected, Hidden)
-        %
-        %   dfref
-        %
-        %       A ``protected`` and ``Hidden`` copy of the user-specified
-        %       input data (or its handle) ``dfref`` to the class constructor.
-        %       This is an exact copy of the user-specified function handle or data.
-        %       Users are supposed to access this component only
-        %       via the class method ``pm.data.DataRef.copy()``.
-        %
+        %>
+        %>  dfref   :   A ``protected`` and ``Hidden`` copy of the user-specified
+        %>              input data (or its handle) ``dfref`` to the class constructor.
+        %>              This is an exact copy of the user-specified function handle or data.
+        %>              Users are supposed to access this component only
+        %>              via the class method ``pm.data.DataRef.copy()``.
+        %>
         dfref;
     end
 
@@ -67,40 +64,39 @@ classdef DataRef < pm.matlab.Handle
 
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+        %>
+        %>  \brief
+        %>  Generate and return a copy of the data (or what is returned by the function handle)
+        %>  given the constructor of the parent object or the data return by the input.
+        %>
+        %>  \details
+        %>  This class method offer the only way to access the user-specified data (reference).
+        %>  The underlying logic behind the use of function to access the data (reference)
+        %>  originates from the lack of the concept of references (pointers)
+        %>  in the MATLAB computing language.
+        %>
+        %>  \return
+        %>  `df`    :   The output scalar MATLAB table a full copy of the data (reference)
+        %>              contained in the user-specified input ``dfref`` passed
+        %>              to the constructor of the parent object.
+        %>
+        %>  \interface{copy}
+        %>  \code{.m}
+        %>
+        %>      df = pm.data.DataRef(dfref);
+        %>      dfcopy = df.copy();
+        %>
+        %>  \endcode
+        %>
+        %>  \see
+        %>  See the parent class [DataRef](@ref DataRef) documentation for example usage.
+        %>
+        %>  \finmain{copy}
+        %>
+        %>  \author
+        %>  \JoshuaOsborne, Friday May 17, 2014, 6:40 PM, The University of Texas at Arlington, Dallas, TX
+        %>  \AmirShahmoradi, Tuesday March 7, 2017, 7:03 AM, Institute for Computational Engineering and Sciences (ICES), The University of Texas at Austin
         function df = copy(self)
-            %
-            %   Generate and return a copy of the data (or what is returned by the function handle)
-            %   given the constructor of the parent object or the data return by the input.
-            %
-            %   This class method offer the only way to access the user-specified data (reference).
-            %   The underlying logic behind the use of function to access the data (reference)
-            %   originates from the lack of the concept of references (pointers)
-            %   in the MATLAB computing language.
-            %
-            %   Parameters
-            %   ----------
-            %
-            %       None
-            %
-            %   Returns
-            %   -------
-            %
-            %       df
-            %
-            %           The output scalar MATLAB table a full copy of the data (reference)
-            %           contained in the user-specified input ``dfref`` passed
-            %           to the constructor of the parent object.
-            %
-            %   Interface
-            %   ---------
-            %
-            %       df = pm.data.DataRef.copy()
-            %
-            %   LICENSE
-            %   -------
-            %
-            %       https://github.com/cdslaborg/paramonte/blob/main/LICENSE.md
-            %
             if  isa(self.dfref, 'function_handle')
                 df = self.dfref();
             else
