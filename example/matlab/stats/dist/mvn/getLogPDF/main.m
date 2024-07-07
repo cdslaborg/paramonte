dfb0c35b02f9f3033addfb1bf379328bfd8f9a04
Amir Shahmoradi <shahmoradi@utexas.edu>
2024-07-07 17:58:45 -0400

    enhancements to MATLAB documentation.

--- example/matlab/stats/dist/mvn/getLogPDF/main.m
+++ example/matlab/stats/dist/mvn/getLogPDF/main.m
@@ -13,11 +13,11 @@ pm.matlab.show('mean = [-3, 3]; cholow = chol([1 -.9; -.9, 1], "lower")')
                 mean = [-3, 3]; cholow = chol([1 -.9; -.9, 1], "lower");
 pm.matlab.show('sample = pm.stats.dist.mvn.getRand(mean, cholow, 1000);')
                 sample = pm.stats.dist.mvn.getRand(mean, cholow, 1000);
-pm.matlab.show("sample = [sample', exp(pm.stats.dist.mvn.getLogPDF(sample, mean, pm.matrix.inv(cholow)))];")
-                sample = [sample', exp(pm.stats.dist.mvn.getLogPDF(sample, mean, pm.matrix.inv(cholow)))];
-pm.matlab.show('p = pm.vis.plot.Scatter3(sample, "colx", 1, "coly", 2, "colz", 3, "colc", 3);')
-                p = pm.vis.plot.Scatter3(sample, "colx", 1, "coly", 2, "colz", 3, "colc", 3);
-pm.matlab.show('p.subplot.xlabel.txt = "X"; p.subplot.ylabel.txt = "Y"; p.subplot.zlabel.txt = "MVN PDF"; p.make();')
-                p.subplot.xlabel.txt = "X"; p.subplot.ylabel.txt = "Y"; p.subplot.zlabel.txt = "MVN PDF"; p.make();
+pm.matlab.show("sample = array2table([sample', exp(pm.stats.dist.mvn.getLogPDF(sample, mean, pm.matrix.inv(cholow)))]);")
+                sample = array2table([sample', exp(pm.stats.dist.mvn.getLogPDF(sample, mean, pm.matrix.inv(cholow)))]);
+pm.matlab.show('sample.Properties.VariableNames = ["X", "Y", "MVN PDF"];')
+                sample.Properties.VariableNames = ["X", "Y", "MVN PDF"];
+pm.matlab.show('p = pm.vis.plot.Scatter3(sample, "colx", 1, "coly", 2, "colz", 3, "colc", 3); p.make();')
+                p = pm.vis.plot.Scatter3(sample, "colx", 1, "coly", 2, "colz", 3, "colc", 3); p.make();
 pm.matlab.show('pm.vis.figure.savefig("mvn.getLogPDF.scatter.3d.png", "-m4");')
                 pm.vis.figure.savefig("mvn.getLogPDF.scatter.3d.png", "-m4");
